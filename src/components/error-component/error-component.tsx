e897ce3f9afcfb4dbbd789b21c1dc787dc526bf3
Izhanitov <dm.define@gmail.com>
2023-05-30 18:25:31 +0700

    Added Error and Loader components

---
+++ src/components/error-component/error-component.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import './styles.scss';
+
+
+const ErrorComponent = () => {    
+    return (
+        <div className='error-component'>
+            Ошибка! Попробуйте перезагрузить страницу или вернитесь к ней позже.
+        </div>
+    )
+}
+
+export default ErrorComponent;
---
+++ src/components/loader/loader.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import './styles.scss';
+
+
+const Loader = () => {
+    return (
+        <div className='loader'>
+            <div className='lds-ring'>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    )
+}
+
+export default Loader;
